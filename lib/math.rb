c65e885ffdf8e7653a13435db592136bb8941b49
Darius Walker <dw862716@ohio.edu>
2020-07-01 16:15:35 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,15 +9,17 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-
+  total = num1/num2
+  return total
 end
 
 def multiplication(num1, num2)
-
+  total = num1*num2
+  return total
 end
 
 def modulo(num1, num2)
-
+  
 end
 
 def square_root(num)
